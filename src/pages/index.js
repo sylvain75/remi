06ad3122073e5cf2c474eb800ea47ae8e5e67386
Sylvain <sylvoz75@gmail.com>
2019-10-12 14:59:10 +1100

    nav definition

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,37 +3,37 @@ import PropTypes from 'prop-types';
 import Layout from 'components/layout';
 import { graphql } from 'gatsby';
 import Box from 'components/box';
-import Title from 'components/title';
 import Gallery from 'components/gallery';
 import IOExample from 'components/io-example';
 import Img from 'gatsby-image';
-// import landingImage from "../../images/landing.jpg"
+
+const styles = {
+  section1: {
+    height: '100vh',
+    width: '100%',
+  },
+  bkImage: {
+    position: 'absolute',
+    top: 0,
+    zIndex: -5,
+    height: '100vh',
+    width: '100%',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    opacity: 0.35,
+  },
+};
 
 const Index = ({ data }) => (
   <Layout>
-    {console.log(data, 'data')}
-    {/*<Box>*/}
-    {/*  <Title as="h2" size="large">*/}
-    {/*    {data.homeJson.content.childMarkdownRemark.rawMarkdownBody}*/}
-    {/*  </Title>*/}
-    {/*    */}
-    {/*</Box>*/}
-    {/*<div*/}
-    {/*  style={{*/}
-    {/*    backgroundImage: `url(${data.homeJson.background.image.childImageSharp.fluid})`,*/}
-    {/*    position: "absolute",*/}
-    {/*    top: 0,*/}
-    {/*    zIndex: -5,*/}
-    {/*    height: "100vh",*/}
-    {/*    width: "100%",*/}
-    {/*    backgroundSize: 'cover',*/}
-    {/*    backgroundPosition: 'center center',*/}
-    {/*    opacity: 0.35,*/}
-    {/*  }}*/}
-    {/*/>*/}
-    <Img fluid={data.homeJson.background.image.childImageSharp.fluid} alt={data.homeJson.background.image.title} />
+    <section style={styles.section1}>
+      <Img fluid={data.homeJson.background.image.childImageSharp.fluid} alt={"hello"} style={styles.bkImage} />
+      <Box>{data.homeJson.content.childMarkdownRemark.rawMarkdownBody}</Box>
+    </section>
+    <section>
+      <Gallery items={data.homeJson.gallery} />
+    </section>
 
-    <Gallery items={data.homeJson.gallery} />
     <div style={{ height: '50vh' }} />
     <IOExample />
   </Layout>
